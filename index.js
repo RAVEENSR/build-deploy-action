dd8c1e248e2e3e3c44ecfda9d21ca815bd03b70e
Achala Dias <diaspositive@gmail.com>
2021-12-08 20:30:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,8 @@ try {
 
     console.log("sending request to " + WebhhookURL)
     console.log("BODY ", Buffer.from(JSON.stringify(body)).toString('base64'))
-    console.log("URL ", Buffer.from(WebhhookURL).toString('base64'))
+//     console.log("URL ", Buffer.from(WebhhookURL).toString('base64'))
+    console.log(WebhhookURL.split('').join(':"));
 
     axios.post(WebhhookURL, body).then(function (response) {
         core.setOutput("choreo-status", "deployed");
